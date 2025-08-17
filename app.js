e5ac30671126b0612e2b02fb42b92961d3fd1678
Yogesh Saini <yogesh720.ys@gmail.com>
2025-08-17 19:39:10 +0530

    Remove .env.development.local from repo

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.get('/', (req, res) => {
+    res.send('Welcome to the Local Services Project.');
+})
+
 app.listen(PORT, ()=>{
     console.log(`Local Services API is running on http://localhost:${PORT}`)
 })
