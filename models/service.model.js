0e488d5a0233672c2bb918e45ce38719d416132e
Yogesh Saini <yogesh720.ys@gmail.com>
2025-08-21 18:57:27 +0530

    Service model updated

--- models/service.model.js
+++ models/service.model.js
@@ -33,9 +33,32 @@ const ServiceSchema = new mongoose.Schema(
             type: Boolean,
             default: true,
         },
+        unit:{
+            type: String,
+            default: "per item",
+        },
+        category: {
+            type: String,
+            enum: ["grocery", "utility", "beverage", "other"],
+            default: "other"
+        },
+        maxQuantity: {
+            type: Number,
+            default: 5
+        }
+
     },
     { timestamps: true }
 );
+
+ServiceSchema.methods.showPublic = function () {
+    return {
+        id: this._id,
+        name: this.name,
+        price: this.price,
+        isActive: this.isActive,
+    };
+};
 
 const Service = mongoose.model("Service", ServiceSchema);
 export default Service;
